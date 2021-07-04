986cf1fa6287ee4929996b314067616a6da497ed
Lloyd Brookes <lloyd.brookes@gmail.com>
2021-07-04 11:01:34 +0100

    workflow

--- test.mjs
+++ test.mjs
@@ -2,36 +2,32 @@ import Tom from 'test-object-model'
 import arrayify from './index.mjs'
 import getAssert from 'isomorphic-assert/index.mjs'
 
-async function getTom () {
-  const a = await getAssert()
-  const tom = new Tom('array-back')
+const a = await getAssert()
+const tom = new Tom('array-back')
 
-  tom.test('if already array, do nothing', function () {
-    const arr = [1, 2, 3]
-    const result = arrayify(arr)
-    a.equal(arr, result)
-  })
+tom.test('if already array, do nothing', function () {
+  const arr = [1, 2, 3]
+  const result = arrayify(arr)
+  a.equal(arr, result)
+})
 
-  tom.test('arrayify()', function () {
-    a.deepEqual(arrayify(undefined), [])
-    a.deepEqual(arrayify(null), [null])
-    a.deepEqual(arrayify(0), [0])
-    a.deepEqual(arrayify([1, 2]), [1, 2])
-    a.deepEqual(arrayify(new Set([1, 2])), [1, 2])
+tom.test('arrayify()', function () {
+  a.deepEqual(arrayify(undefined), [])
+  a.deepEqual(arrayify(null), [null])
+  a.deepEqual(arrayify(0), [0])
+  a.deepEqual(arrayify([1, 2]), [1, 2])
+  a.deepEqual(arrayify(new Set([1, 2])), [1, 2])
 
-    function func () {
-      a.deepEqual(arrayify(arguments), [1, 2, 3])
-    }
-    func(1, 2, 3)
+  function func () {
+    a.deepEqual(arrayify(arguments), [1, 2, 3])
+  }
+  func(1, 2, 3)
 
-    a.deepEqual(arrayify({ one: 1 }), [{ one: 1 }])
-    const map = new Map()
-    map.set('one', 1)
-    map.set('two', 2)
-    a.deepEqual(arrayify(map), [map])
-  })
+  a.deepEqual(arrayify({ one: 1 }), [{ one: 1 }])
+  const map = new Map()
+  map.set('one', 1)
+  map.set('two', 2)
+  a.deepEqual(arrayify(map), [map])
+})
 
-  return tom
-}
-
-export default getTom()
+export default tom
